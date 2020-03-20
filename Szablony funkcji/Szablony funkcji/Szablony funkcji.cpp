b3306d4ff0091645f9c504121912809a5443cc64
zeref80 <45498824+zeref80@users.noreply.github.com>
2020-03-20 11:51:40 +0100

    Szablony funkcji
    kod z maila

---
+++ Szablony funkcji/Szablony funkcji/Szablony funkcji.cpp
@@ -0,0 +1,35 @@
+﻿// funtemp.cpp -- użycie szablonu funkcji
+#include <iostream>
+// prototyp szablonu funkcji
+template <typename T> // albo class T
+void Swap(T& a, T& b);
+int main()
+{
+	setlocale(LC_ALL, "pl_PL");
+	using namespace std;
+	int i = 10;
+	int j = 20;
+	cout << "i, j = " << i << ", " << j << ".\n";
+	cout << "Użycie funkcji obsługującej typ int, "
+		"wygenerowanej przez kompilator:\n";
+	Swap(i, j); // generuje void Swap(int &, int &)
+	cout << "Teraz i, j = " << i << ", " << j << ".\n";
+	double x = 24.5;
+	double y = 81.7;
+	cout << "x, y = " << x << ", " << y << ".\n";
+	cout << "Użycie funkcji obsługującej typ double, "
+		"wygenerowanej przez kompilator:\n";
+	Swap(x, y); // generuje void Swap(double &, double &)
+	cout << "Teraz x, y = " << x << ", " << y << ".\n";
+	// cin.get();
+	return 0;
+}
+// definicja szablonu funkcji
+template <typename T> // lub class T
+void Swap(T& a, T& b)
+{
+	T temp; // zmienna temp typu T
+	temp = a;
+	a = b;
+	b = temp;
+}
